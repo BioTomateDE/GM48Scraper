ae7e0b46d15f12fd1fe7c93fb79170c456997f23
BioTomateDE <latus.kati@gmail.com>
2026-03-13 20:42:07 +0000

    cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -4,15 +4,15 @@ use std::{num::NonZeroU32, path::PathBuf};
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    #[arg(default_value = "gm48_datafiles")]
     /// Directory the data files will be downloaded to.
+    #[arg(default_value = "gm48_datafiles")]
     pub directory: PathBuf,
 
-    #[arg(short, long, default_value = "6")]
     /// How many requests can be sent at once.
     ///
     /// Higher values may speed up the program,
     /// but at a certain point the server will block requests due to spam.
+    #[arg(short, long, default_value = "6")]
     pub jobs: NonZeroU32,
 }
 
